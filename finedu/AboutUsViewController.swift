903ada6e1fd595f6a4665269376c56db6f2def27
Huy Le <huylx86@gmail.com>
2017-03-28 03:53:12 +0700

    - fix CR convert Home and About Us to native

--- finedu/AboutUsViewController.swift
+++ finedu/AboutUsViewController.swift
@@ -9,27 +9,124 @@
 import UIKit
 
 
-class AboutUsViewController: UIViewController, UIWebViewDelegate {
+class AboutUsViewController: UIViewController {
     
-    @IBOutlet weak var webView : UIWebView!
-    @IBOutlet weak var progressBar : UIImageView!
+    /*@IBOutlet weak var webView : UIWebView!
+    @IBOutlet weak var progressBar : UIImageView!*/
+    @IBOutlet weak var text1 : UITextView!
+    @IBOutlet weak var text2 : UITextView!
+    @IBOutlet weak var btnLearnMode : UILabel!
+    @IBOutlet weak var imgLogo : UIImageView!
+    
+    let images = [
+        UIImage(named: "nanyang")!,
+        UIImage(named: "deloitte")!,
+        UIImage(named: "sias")]
+    var index = 0
+    var currentIndex = 0
+    let animationDuration: TimeInterval = 0.25
+    let switchingInterval: TimeInterval = 3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = NSURL (string: "http://finedu.wmi.com.sg/credits/");
+        btnLearnMode.layer.borderWidth = 4
+        btnLearnMode.layer.cornerRadius = 5
+        btnLearnMode.layer.borderColor = UIColor(red:203/255.0, green:178/255.0, blue:52/255.0, alpha: 1.0).cgColor
+        
+        
+        text1.font = updateTextFont(textView: text1);
+        text2.font = updateTextFont(textView: text2);
+        
+        index += 1;
+        currentIndex = index
+        imgLogo.image = images[index];
+        animateImageView();
+        
+        let guestureLearnMode = UITapGestureRecognizer(target: self, action: #selector(self.learnMoreClick(_:)));
+        btnLearnMode.addGestureRecognizer(guestureLearnMode)
+        
+        let guestureLogo = UITapGestureRecognizer(target: self, action: #selector(self.logoClick(_:)));
+        imgLogo.addGestureRecognizer(guestureLogo)
+        
+        /*let url = NSURL (string: "http://finedu.wmi.com.sg/credits/");
         let requestObj = NSURLRequest(url: url! as URL);
         webView.delegate = self
         webView.loadRequest(requestObj as URLRequest);
-        progressBar.image = UIImage.gifImageWithName("loading")
+        progressBar.image = UIImage.gifImageWithName("loading")*/
         
     }
     
-    func webViewDidStartLoad(_ webView: UIWebView) {
+    func updateTextFont(textView : UITextView) -> UIFont {
+        var fontSize = textView.font?.pointSize;
+        let minSize : CGFloat = 12;
+        var size : CGSize = textView.sizeThatFits(CGSize(width: textView.frame.width, height: 10000));
+        while (fontSize! > minSize &&  size.height >= textView.frame.size.height ) {
+            fontSize = fontSize! - 1.0;
+            textView.font = UIFont(name: (textView.font?.fontName)!, size: fontSize!)
+            size = textView.sizeThatFits(CGSize(width: textView.frame.width, height: 10000));
+        }
+        return textView.font!;
+    }
+    
+    func animateImageView() {
+        CATransaction.begin()
+        
+        CATransaction.setAnimationDuration(animationDuration)
+        CATransaction.setCompletionBlock {
+            let delay = DispatchTime.now() + self.switchingInterval;
+            DispatchQueue.main.asyncAfter(deadline:delay) {
+                self.animateImageView()
+            }
+        }
+    
+        let transition = CATransition()
+//        transition.type = kCATransitionFade
+        
+        transition.type = kCATransitionPush
+        transition.subtype = kCATransitionFromRight
+        
+        imgLogo.layer.add(transition, forKey: nil)
+        imgLogo.image = images[index]
+        currentIndex = index
+        CATransaction.commit()
+        
+        index = index < images.count - 1 ? index + 1 : 0
+    }
+    
+    func learnMoreClick(_ sender : UITapGestureRecognizer)
+    {
+        let url = NSURL(string: "http://www.wmi.com.sg")!
+        UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+    }
+    
+    func logoClick(_ sender : UITapGestureRecognizer)
+    {
+        var strUrl = ""
+        switch(currentIndex){
+        case 0 :
+            strUrl = "http://www.ntu.edu.sg/Pages/home.aspx"
+            break;
+        case 1:
+            strUrl = "https://www2.deloitte.com/sg/en.html"
+            break;
+        case 2:
+            strUrl = "https://sias.org.sg/"
+            break;
+        default:
+            strUrl = "https://sias.org.sg/"
+            break;
+        }
+        
+        let url = NSURL(string: strUrl)
+        UIApplication.shared.open(url as! URL, options: [:], completionHandler: nil)
+    }
+    
+    /*func webViewDidStartLoad(_ webView: UIWebView) {
         progressBar.isHidden = false
         
     }
     func webViewDidFinishLoad(_ webView: UIWebView) {
         progressBar.isHidden = true
-    }
+    }*/
 }
--- finedu/HomeViewController.swift
+++ finedu/HomeViewController.swift
@@ -8,29 +8,91 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UIWebViewDelegate {
+class HomeViewController: UIViewController {
     
-    @IBOutlet weak var webView : UIWebView!
-    @IBOutlet weak var progressBar : UIImageView!
+    /*@IBOutlet weak var webView : UIWebView!
+    @IBOutlet weak var progressBar : UIImageView!*/
+    @IBOutlet weak var view1 : UIView!
+    @IBOutlet weak var view2 : UIView!
+    @IBOutlet weak var view3 : UIView!
+    @IBOutlet weak var view4 : UIView!
+    @IBOutlet weak var view5 : UIView!
+    
+    @IBOutlet weak var scrollview : UIScrollView!
+    
+    @IBOutlet weak var textTitle1 : UITextView!
+    @IBOutlet weak var textContent1 : UITextView!
+    @IBOutlet weak var textTitle2 : UITextView!
+    @IBOutlet weak var textContent2 : UITextView!
+    @IBOutlet weak var textTitle3 : UITextView!
+    @IBOutlet weak var textContent3 : UITextView!
+    @IBOutlet weak var textTitle4 : UITextView!
+    @IBOutlet weak var textContent4 : UITextView!
+    @IBOutlet weak var textTitle5 : UITextView!
+    @IBOutlet weak var textContent5 : UITextView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = NSURL (string: "http://finedu.wmi.com.sg/mobile/");
-
-        let requestObj = NSURLRequest(url: url! as URL);
-        webView.delegate = self
-        webView.loadRequest(requestObj as URLRequest);
-        progressBar.image = UIImage.gifImageWithName("loading")
+        textTitle1.font = updateTextFont(textView: textTitle1);
+        textContent1.font = updateTextFont(textView: textContent1);
+        textTitle2.font = updateTextFont(textView: textTitle2);
+        textContent2.font = updateTextFont(textView: textContent2);
+        textTitle3.font = updateTextFont(textView: textTitle3);
+        textContent3.font = updateTextFont(textView: textContent3);
+        textTitle4.font = updateTextFont(textView: textTitle4);
+        textContent4.font = updateTextFont(textView: textContent4);
+        textTitle5.font = updateTextFont(textView: textTitle5);
+        textContent5.font = updateTextFont(textView: textContent5);
+        
+        adjustView();
+        scrollview.contentSize.height = view5.frame.height + view5.frame.origin.y + 24;
+        
+//        
+//        let url = NSURL (string: "http://finedu.wmi.com.sg/mobile/");
+//
+//        let requestObj = NSURLRequest(url: url! as URL);
+//        webView.delegate = self
+//        webView.loadRequest(requestObj as URLRequest);
+//        progressBar.image = UIImage.gifImageWithName("loading")
     
     }
     
-    func webViewDidStartLoad(_ webView: UIWebView) {
+    /*func webViewDidStartLoad(_ webView: UIWebView) {
         progressBar.isHidden = false
         
     }
     func webViewDidFinishLoad(_ webView: UIWebView) {
         progressBar.isHidden = true
+    }*/
+    
+    func updateTextFont(textView : UITextView) -> UIFont {
+        var fontSize = textView.font?.pointSize;
+        let minSize : CGFloat = 12;
+        var size : CGSize = textView.sizeThatFits(CGSize(width: textView.frame.width, height: 10000));
+        while (fontSize! > minSize &&  size.height >= textView.frame.size.height ) {
+            fontSize = fontSize! - 1.0;
+            textView.font = UIFont(name: (textView.font?.fontName)!, size: fontSize!)
+            size = textView.sizeThatFits(CGSize(width: textView.frame.width, height: 10000));
+        }
+        return textView.font!;
     }
     
+    func adjustView() {
+        
+        view1.frame = CGRect(x:view1.frame.origin.x, y: view1.frame.origin.y, width: view1.frame.width, height:textContent1.frame.height + textContent1.frame.origin.y);
+        view2.frame = CGRect(x:view2.frame.origin.x, y: view2.frame.origin.y, width: view2.frame.width, height:textContent2.frame.height + textContent2.frame.origin.y);
+        view3.frame = CGRect(x:view3.frame.origin.x, y: view3.frame.origin.y, width: view3.frame.width, height:textContent3.frame.height + textContent3.frame.origin.y);
+        view4.frame = CGRect(x:view4.frame.origin.x, y: view4.frame.origin.y, width: view4.frame.width, height:textContent4.frame.height + textContent4.frame.origin.y);
+        view5.frame = CGRect(x:view5.frame.origin.x, y: view5.frame.origin.y, width: view5.frame.width, height:textContent5.frame.height + textContent5.frame.origin.y);
+        let frmView1 = view1.frame;
+        view2.frame.origin.y = frmView1.size.height + frmView1.origin.y;
+        let frmView2 = view2.frame;
+        view3.frame.origin.y = frmView2.size.height + frmView2.origin.y;
+        let frmView3 = view3.frame;
+        view4.frame.origin.y = frmView3.size.height + frmView3.origin.y;
+        let frmView4 = view4.frame;
+        view5.frame.origin.y = frmView4.size.height + frmView4.origin.y;
+    }
 }
